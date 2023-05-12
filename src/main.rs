4afc7dc7a7dc4ae61b10e751ac4af9a9d22e4a9c
Matt Roberts <matt.roberts@discogsinc.com>
2023-05-11 21:53:02 -0700

    Adds tracing for observability

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
 use std::net::TcpListener;
 
-use env_logger::Env;
 use sqlx::PgPool;
+use tracing::subscriber::{Subscriber, set_global_default};
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
+pub fn get_subscriber(name: String, env: String) -> impl Subscriber + Send + Sync {
+    let filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new(env));
+    let formatting_layer = BunyanFormattingLayer::new(
+        name,
+        std::io::stdout
+    );
+    Registry::default()
+        .with(filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
+}
+
+pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+    LogTracer::init().expect("Failed to set logger.");
+    set_global_default(subscriber).expect("Failed to set subscriber.");
+}
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // Logging
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
+    
+    // App Config
     let config = get_configuration().expect("Failed to read config.");
     let connection = PgPool::connect(&config.database.connection_string())
         .await
