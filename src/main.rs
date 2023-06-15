8c75c092041f14a80fad0662dee281be0eeaf575
Matt Roberts <matt.roberts@discogsinc.com>
2023-06-14 17:37:55 -0700

    Add email client and tests
    Tests are currently failing, but they leverage faker, which is pretty
    dope. Also makes use of `wiremock` for HTTP requests.
    
    Completes section 7.2.3

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::net::TcpListener;
 use sqlx::postgres::PgPoolOptions;
 
 use zero2prod::configuration::get_configuration;
+use zero2prod::email_client::EmailClient;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
@@ -17,7 +18,14 @@ async fn main() -> Result<(), std::io::Error> {
     let connection = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(config.database.with_db());
+
+    let send_email = config.email_client.sender().expect("Invalid sender email address.");
+    let email_client = EmailClient::new(
+        config.email_client.base_url,
+        send_email,
+    );
+
     let address = format!("{}:{}", config.application.host, config.application.port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection)?.await
+    run(listener, connection, email_client)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -5,6 +5,7 @@ use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
+use zero2prod::email_client::EmailClient;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
@@ -38,7 +39,14 @@ async fn spawn_app() -> TestApp {
     config.database.database_name = Uuid::new_v4().to_string();
     let pool = configure_database(&config.database).await;
 
-    let server = run(listener, pool.clone()).expect("Failed to bind address$");
+    let sender_email = config.email_client.sender().expect("msnvalid sender email address.");
+    let email_client = EmailClient::new(
+        config.email_client.base_url,
+        sender_email
+    );
+
+    let server = run(listener, pool.clone(), email_client)
+        .expect("Failed to bind address$");
     let _ = tokio::spawn(server);
 
     TestApp {
