711a76ff387c6d91302d5beb2ea3cbc404e34a2a
Matt Roberts <matt.roberts@discogsinc.com>
2023-07-11 18:26:35 -0700

    Add link to confirmation email
    Test passes. Completes section 7.7.2

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -44,13 +44,7 @@ pub async fn subscribe(
         HttpResponse::InternalServerError().finish();
     }
 
-    if email_client
-        .send_email(
-            new_subscriber.email,
-            "Welcome!",
-            "Welcome to our newsletter!",
-            "Welcome to our newsletter!",
-        )
+    if send_confirmation_email(&email_client, new_subscriber)
         .await
         .is_err()
     {
@@ -87,4 +81,31 @@ pub async fn insert_subscriber(
         e
     })?;
     Ok(())
+}
+
+#[tracing::instrument(
+    name = "Send a confirmation email to a new subscriber",
+    skip(email_client, new_subscriber)
+)]
+pub async fn send_confirmation_email(email_client: &EmailClient, new_subscriber: NewSubscriber) -> Result<(), reqwest::Error> {
+    let confirmation_link =
+        "https://there-is-no-such-domain.com/subscriptions/confirm";
+    let text_body = format!(
+        "Welcome to our newsletter!<br /> Visit {} to confirm your subscription.",
+        confirmation_link
+    );
+    let html_body = &format!(
+        "Welcome to our newsletter!<br />
+        Click <a href=\"{}\">here</a> to confirm your subscription.",
+        confirmation_link
+    );
+
+    email_client
+        .send_email(
+            new_subscriber.email,
+            "Welcome!",
+            &html_body,
+            &text_body
+        )
+        .await
 }
