984bcb81c6007a1c4a9011ea37f7739c786c28d9
Matt Roberts <matt.roberts@discogsinc.com>
2023-07-12 17:07:41 -0700

    Test record is persisted with correct status
    Ran into an issue with actix running out of file handles during tests.
    Solution was to run `ulimit -n 2048` to increase the file handle limit,
    but this does not persist on reboot. Further, Ventura should have a much
    higher limit, but something is off. Will dig into it later.
    
    Completes section 7.7.3

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -67,7 +67,7 @@ pub async fn insert_subscriber(
         INSERT INTO subscriptions
         (id, email, name, subscribed_at, status)
         VALUES
-        ($1, $2, $3, $4, 'confirmed')
+        ($1, $2, $3, $4, 'pending_confirmation')
     "#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
