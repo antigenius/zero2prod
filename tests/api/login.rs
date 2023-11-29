b4d40376f8e31fdb16aeb30f18222dc704b7b44c
Matt Roberts <matt.roberts@discogsinc.com>
2023-11-28 19:15:23 -0800

    Add cookie bits for flash messaging
    Completes section 10.6.4.10

---
+++ tests/api/login.rs
@@ -0,0 +1,35 @@
+use std::collections::HashSet;
+
+use reqwest::header::HeaderValue;
+
+use crate::helpers::{assert_is_redirected_to, spawn_app};
+
+
+#[tokio::test]
+async fn an_error_flash_message_is_set_on_failure() {
+    let app = spawn_app().await;
+
+    let login_body = serde_json::json!({
+        "username": "random-username",
+        "password": "random-password"
+    });
+    let response = app.post_login(&login_body).await;
+    let flash_cookie = response
+        .cookies()
+        .find(|c| c.name() == "_flash")
+        .unwrap();
+    let cookies: HashSet<_> = response
+        .headers()
+        .get_all("Set-Cookie")
+        .into_iter()
+        .collect();
+
+    assert_is_redirected_to(&response, "/login");
+    assert!(cookies
+        .contains(
+            &HeaderValue::from_str("_flash=Authentication failed.")
+                .unwrap()
+        )
+    );
+    assert_eq!(flash_cookie.value(), "Authentication failed.");
+}
